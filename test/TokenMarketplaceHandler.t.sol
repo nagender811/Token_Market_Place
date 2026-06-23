a614367c94ea550d995b4bd62991a1b281bc4e24
Nagender kumar <kumarnagender811@gmail.com>
2026-06-23 13:11:45 +0530

    add createSellOrder function to Test

--- test/TokenMarketplaceHandler.t.sol
+++ test/TokenMarketplaceHandler.t.sol
@@ -30,4 +30,18 @@ contract MarketplaceHandler is Test {
 
         marketplaceTokensBought+=amount;
     }
+
+    function createSellOrder(uint256 amount) public {
+        uint256 sellerBalance = token.balanceOf(seller);
+        if (sellerBalance == 0) return;
+        
+        amount = bound(amount, 1, sellerBalance);
+
+        vm.startPrank(seller); 
+        token.approve(address(marketplace), amount);
+        marketplace.createSellOrder(amount);
+        vm.stopPrank();
+
+        openOrderTokens += amount;
+    }
 }
