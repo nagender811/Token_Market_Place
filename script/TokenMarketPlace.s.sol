dcf095ccfd98f44118b21461189eb6fcd4258216
Nagender kumar <kumarnagender811@gmail.com>
2026-06-26 20:53:22 +0530

    fix: update TokenMarketplace import and initialize with network configuration

--- script/TokenMarketPlace.s.sol
+++ script/TokenMarketPlace.s.sol
@@ -2,18 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {TokenMarketplace} from "../src/Counter.sol";
+import {TokenMarketplace} from "../src/TokenMarketPlace.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract TokenMarketplaceScript is Script {
-    TokenMarketplace public tokenMarketplace;
-
-    function setUp() public {}
-
     function run() public {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
         vm.startBroadcast();
-
-        tokenMarketplace = new TokenMarketplace();
-
+        TokenMarketplace tokenMarketplace = new TokenMarketplace(config.initialOwner, config.slvToken);
         vm.stopBroadcast();
     }
 }
+
