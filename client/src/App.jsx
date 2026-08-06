d55339f35efacba8cf5d158bd7599f7604a4a9c8
Nagender kumar <kumarnagender811@gmail.com>
2026-08-06 11:15:37 +0530

    Configured Vite for the project.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+// import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <button onClick={connectWallet}>Connect Wallet</button>
+    </>
+  );
+}
+
+export default App;
