4d486d05add5a4945f420ededed3d8a83de3dd28
Nagender kumar <kumarnagender811@gmail.com>
2026-08-06 13:31:50 +0530

    feat: implement wallet connection functionality and display connected address

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,21 @@ import { useState } from "react";
 // import './App.css'
 
 function App() {
- 
+  const [address, setAddress] = useState("");
+  async function connectWallet() {
+    if (!window.ethereum) {
+      alert("Ethereum Wallet is not Installed");
+    } else {
+      const addresses = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAddress(addresses[0]);
+    }
+  }
   return (
     <>
       <button onClick={connectWallet}>Connect Wallet</button>
+      <p>Connected Address: {address}</p>
     </>
   );
 }
