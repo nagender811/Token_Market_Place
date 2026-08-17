2f0e1ca3746c8200140b42116e2bbf6efbda7d65
Nagender kumar <kumarnagender811@gmail.com>
2026-08-17 13:42:21 +0530

    feat: enhance TokenMarketplaceScript with deployment logging

--- script/TokenMarketPlace.s.sol
+++ script/TokenMarketPlace.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
 import {TokenMarketplace} from "../src/TokenMarketPlace.sol";
+import {console2} from "forge-std/console2.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract TokenMarketplaceScript is Script {
@@ -11,8 +12,14 @@ contract TokenMarketplaceScript is Script {
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
         vm.startBroadcast();
-        new TokenMarketplace(config.initialOwner, config.slvToken);
+        TokenMarketplace tokenMarketplace = new TokenMarketplace(
+            config.initialOwner,
+            config.slvToken
+        );
         vm.stopBroadcast();
+        console2.log(
+            "TokenMarketplace deployed at:",
+            address(tokenMarketplace)
+        );
     }
 }
-
