b90ff8b802b3b97cae55c500717c01f70a650082
Nagender kumar <kumarnagender811@gmail.com>
2026-08-17 13:42:35 +0530

    feat: add contract initialization and state management for wallet connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-// import './App.css'
+import { ethers } from "ethers";
+import abi from "../src/contractAbi/tokenMarketplaceAbi.json";
 
 function App() {
   const [address, setAddress] = useState("");
+  const [contract, setContract] = useState("");
+
   async function connectWallet() {
     if (!window.ethereum) {
       alert("Ethereum Wallet is not Installed");
@@ -10,6 +13,14 @@ function App() {
       const addresses = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
+
+      const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+
+      setContract(contract);
       setAddress(addresses[0]);
     }
   }
