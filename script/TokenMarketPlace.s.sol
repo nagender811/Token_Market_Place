58585081018ba0e8ca052acf2bb319bd2299f9bd
Nagender kumar <kumarnagender811@gmail.com>
2026-09-08 19:44:48 +0530

    feat: add initial token minting functionality in TokenMarketplaceScript

--- script/TokenMarketPlace.s.sol
+++ script/TokenMarketPlace.s.sol
@@ -5,8 +5,10 @@ import {Script} from "forge-std/Script.sol";
 import {TokenMarketplace} from "../src/TokenMarketPlace.sol";
 import {console2} from "forge-std/console2.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract TokenMarketplaceScript is Script {
+    uint256 private constant INITIAL_MARKETPLACE_TOKENS = 1_000;
     function run() public {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
@@ -16,6 +18,9 @@ contract TokenMarketplaceScript is Script {
             config.slvToken,
             config.initialOwner
         );
+        if (block.chainid == helperConfig.LOCAL_CHAIN_ID()) {
+            ERC20Mock(config.slvToken).mint(address(tokenMarketplace), INITIAL_MARKETPLACE_TOKENS);
+        }
         vm.stopBroadcast();
         console2.log(
             "TokenMarketplace deployed at:",
